c744a754e3451c80ff30b58ccc602f8125b6dde0
olaitan <aranmolateolaitan1@gmail.com>
2023-08-25 15:27:53 +0100

    first commit

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ window.onscroll = function() {
         nav.classList.remove('sticky');
     }
 };
+const hamburger = document.getElementById ("hamburger")
+const collapsed_menu = document.getElementById ("collapsed-menu")
+const close = document.getElementById ("close")
+hamburger.addEventListener("click", ()=> {
+    collapsed_menu.classList.toggle('hidden')
+    hamburger.classList.toggle('hidden')
+    close.classList.toggle('hidden')
+})
+close.addEventListener("click", ()=> {
+    collapsed_menu.classList.toggle('hidden')
+    hamburger.classList.toggle('hidden')
+    close.classList.toggle('hidden')
+})
