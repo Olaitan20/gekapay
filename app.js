2264c2fcf513da4dbca90d7d535fee2caef66ece
olaitan <aranmolateolaitan1@gmail.com>
2023-08-25 05:52:13 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('sticky');
+    } else {
+        nav.classList.remove('sticky');
+    }
+};
